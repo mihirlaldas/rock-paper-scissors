6e65ca5c8e7572c5121fe4b6e02ff0418944c84c
mihirlaldas <mihirlaldas@gmail.com>
2024-03-31 23:35:56 +0530

    show timeout button

---
+++ src/utils/invokeTimeout.ts
@@ -0,0 +1,47 @@
+import { ethers } from "ethers";
+import Contract from "./RPS.json";
+
+export async function invokeJ2Timeout() {
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const signer = await provider.getSigner();
+  const rpsContractAddress = localStorage
+    .getItem("rps-contract-address")
+    ?.replace(/['"]+/g, "") as string;
+  console.log(rpsContractAddress);
+  const rpsContract = new ethers.Contract(
+    rpsContractAddress,
+    Contract.abi,
+    signer
+  );
+  try {
+    const c2 = await rpsContract.c2();
+    if (!c2) {
+      const tx = await rpsContract.j2Timeout({ estimateGas: 300000 });
+      await tx.wait();
+    }
+  } catch (error) {
+    alert(error);
+  }
+}
+export async function invokeJ1Timeout() {
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const signer = await provider.getSigner();
+  const rpsContractAddress = localStorage
+    .getItem("rps-contract-address")
+    ?.replace(/['"]+/g, "") as string;
+  const rpsContract = new ethers.Contract(
+    rpsContractAddress,
+    Contract.abi,
+    signer
+  );
+  try {
+    const contractStake = await rpsContract.stake();
+    if (contractStake > 0) {
+      const tx = await rpsContract.j1Timeout({ estimateGas: 300000 });
+      await tx.wait();
+    }
+    throw new Error("Stake is present, cannot execute j1Timeout");
+  } catch (error) {
+    alert("Error while invoking j1Timeout function :");
+  }
+}
