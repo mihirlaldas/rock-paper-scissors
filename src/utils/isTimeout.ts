25a4d2cc0aff521653a54666ff0eb15017b1e5e0
mihirlaldas <mihirlaldas@gmail.com>
2024-04-01 09:35:36 +0530

    date format

--- src/utils/isTimeout.ts
+++ src/utils/isTimeout.ts
@@ -5,8 +5,8 @@ export function isTimeout(lastAction: BigNumberish): boolean {
   if (!lastAction) {
     return false;
   }
-  const lastActionDate = new Date(ethers.toNumber(lastAction) * 1000);
-  const presentDate = new Date();
+  const lastActionDate = new Date(ethers.toNumber(lastAction) * 1000).getTime();
+  const presentDate = new Date().getTime();
   const FIVE_MINUTES = 5 * 60 * 1000;
   const isTimedout = presentDate - lastActionDate > FIVE_MINUTES;
   return isTimedout;
